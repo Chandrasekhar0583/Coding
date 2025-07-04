1f8c6ec59d846714f44852475dcb84af3bbd981a
CHANDRASEKHAR MAJJI <142887761+Chandrasekhar0583@users.noreply.github.com>
2025-07-04 21:27:16 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (45.12%) - LeetSync

---
+++ leetcode/3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public char kthCharacter(long k, int[] arr) {
+        long count = 0;
+        int n  = arr.length,i;
+        long pow = 1;
+        for( i = 0 ; i <= n ; i++)
+        {
+            pow = (1L << i);
+            if(pow >= k)
+            break;
+        }
+        count = fun( pow - (pow - k) , arr , i , pow);
+        return (char)('a' + (count % 26));
+    }
+    public long fun(long k , int[] arr , int ind , long pow)
+    {
+        if(ind == 0 || k == 1)
+        {
+            return 0;
+        }
+        if(ind == 1)
+        {
+            return arr[0];
+        }
+        long half = pow / 2;
+        if(k <= half)
+        return fun( k  , arr , ind - 1, half);
+        return arr[ind  - 1] + fun( k - half , arr , ind - 1, half );
+    }
+}
