69db553b1341d6811e77b7c8d03934c0163fca6e
CHANDRASEKHAR MAJJI <142887761+Chandrasekhar0583@users.noreply.github.com>
2025-07-12 22:15:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Mark Even and Odd - Python/mark-even-and-odd-python.py
@@ -0,0 +1,8 @@
+def checkOddEven(x):
+    if (x % 2 == 0 and x != 1):
+        # Complete the statement below
+        return True
+
+    else:
+        # Complete the statement below
+        return False
