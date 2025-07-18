df72a8acf9305cb63fdaf0559e4bc7b61d47d8a6
CHANDRASEKHAR MAJJI <142887761+Chandrasekhar0583@users.noreply.github.com>
2025-07-18 22:36:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print Linked List/print-linked-list.java
+++ Difficulty: Basic/Print Linked List/print-linked-list.java
@@ -1,55 +1,3 @@
-//{ Driver Code Starts
-import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
-
-class LinkedList {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();
-        scanner.nextLine(); // Consume newline character after t
-
-        while (t-- > 0) {
-            String input =
-                scanner.nextLine(); // Read the entire line for the array elements
-            Scanner lineScanner = new Scanner(input);
-            Node head = null, tail = null;
-            int x;
-
-            // Building the linked list from input
-            while (lineScanner.hasNextInt()) {
-                x = lineScanner.nextInt();
-                if (head == null) {
-                    head = new Node(x);
-                    tail = head;
-                } else {
-                    tail.next = new Node(x);
-                    tail = tail.next;
-                }
-            }
-            lineScanner.close();
-
-            Solution ob = new Solution();
-            ob.printList(head);
-            System.out.println();
-
-            System.out.println("~");
-        }
-        scanner.close();
-    }
-}
-
-// } Driver Code Ends
-
-
 /* Node is defined as
 class Node {
     int data;
@@ -68,12 +16,11 @@ class Solution {
     // Function to display the elements of a linked list in same line
     void printList(Node head) {
         // add code here.
-        Node temp = head ;
-        while(temp != null)
+        Node cur = head;
+        while(cur != null)
         {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
+            System.out.print(cur.data+" ");
+            cur = cur.next;
         }
-        // System.out.println();
     }
 }
